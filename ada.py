830934425731b24d9b40ab4c10a23c79050c3162
gonzalopelizzari <gpelizzari@mail.austral.edu.ar>
2025-04-10 11:57:34 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name = first_name +" "+ last_name
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+    print(f" \t{name.lower()} ")
